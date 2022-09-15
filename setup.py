9b2e2e630fdc6382874a7c66245f13d57741a70e
eduardodut <eduardo_dut@edu.unifor.br>
2022-09-14 23:31:59 -0300

    .

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='pipeline_utils', version='1.0', packages=find_packages())
+setup(name='Pipelines', version='1.0', packages=find_packages())
